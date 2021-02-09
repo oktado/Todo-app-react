c52b40ad1ed21590a7df6b43ed54e0982f229b62
Oktado Putra <oktado.putra@gmail.com>
2021-02-09 22:44:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import moment from "moment";
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //Run once
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  //Save to localstorage
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  //check localstorage
+  const getLocalTodos = () => {
+    //check if localstorage has already set or not. if not it will set and make empty array
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos")); //get todos in localstorage
+      setTodos(todoLocal); //set todo from localstorage into current todo state
+    }
+  };
+
+  //set time
+  const time = moment().format("MMMM Do YYYY");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Whats Your Plan Today ?</h1>
       </header>
+      <h3 className="time">{time}</h3>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setInputText={setInputText}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Form = ({ inputText, setInputText, setTodos, todos, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.random() * 1000,
+      },
+    ]);
+    setInputText("");
+  };
+
+  const StatusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <form>
+      <input
+        onChange={inputTextHandler}
+        value={inputText}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={StatusHandler} name="todos" className="filter-todo">
+          <option value="all">all</option>
+          <option value="completed">completed</option>
+          <option value="uncompleted">uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
